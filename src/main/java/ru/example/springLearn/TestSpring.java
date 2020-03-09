955a2c014b137b166381f6fdad3724e5ee33f14a
Aliaksei Dauhapolau <aliaksei.dauhapolau@gmail.com>
2020-03-09 23:03:55 +0300

    Scope Singleton,Prototype

--- src/main/java/ru/example/springLearn/TestSpring.java
+++ src/main/java/ru/example/springLearn/TestSpring.java
@@ -29,16 +29,24 @@ public class TestSpring {
 
         //dependency injection
         MusicPlayer musicPlayer1 = context.getBean("musicPlayerClassic", MusicPlayer.class);
+        musicPlayer1.setVolume(99);
         musicPlayer1.playMusic();
+        System.out.println(musicPlayer1.getVolume());
 
+        MusicPlayer musicPlayer5 = context.getBean("musicPlayerClassic", MusicPlayer.class);
+        musicPlayer5.setVolume(22);
+        System.out.println(musicPlayer5.getVolume());
 
         MusicPlayer musicPlayer2 = context.getBean("musicPlayerRock", MusicPlayer.class);
         musicPlayer2.playMusic();
         //context.close();
+        boolean comparison = musicPlayer1 == musicPlayer5;
+        System.out.println(comparison);
 
         //dependency injection with setter/getter
         MusicPlayer musicPlayer3 = context.getBean("musicPlayerClassicSetter", MusicPlayer.class);
         musicPlayer3.playMusic();
+
         System.out.println(musicPlayer3.getName());
         System.out.println(musicPlayer3.getVolume());
         //context.close();
