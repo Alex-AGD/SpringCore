9a7cefe3d8d6401c9993ee2c4510810ff218ee3c
Aliaksei Dauhapolau <aliaksei.dauhapolau@gmail.com>
2020-03-09 23:17:44 +0300

    :@

--- src/main/java/ru/example/springLearn/ClassicalMusic.java
+++ src/main/java/ru/example/springLearn/ClassicalMusic.java
@@ -1,6 +1,13 @@
 package ru.example.springLearn;
 
 public class ClassicalMusic implements Music{
+    public void doMyInit(){
+        System.out.println("Doing my initialization");
+    }
+
+    public void doMyDestroy(){
+        System.out.println("Doing my destruction");
+    }
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
