2003d7f4c5989bbbbbe831df71137459a2622f43
Aliaksei Dauhapolau <aliaksei.dauhapolau@gmail.com>
2020-03-09 07:00:35 +0300

    Dependency injection (part2) properties

--- src/main/java/ru/example/springLearn/MusicPlayer.java
+++ src/main/java/ru/example/springLearn/MusicPlayer.java
@@ -2,15 +2,43 @@ package ru.example.springLearn;
 
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
 
+    public Music getMusic() {
+        return music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
 
     //IoC
     public MusicPlayer(Music music) {
         this.music = music;
     }
 
-public void playMusic(){
-    System.out.println("Playing: " + music.getSong());
-}
+    public MusicPlayer(){}
+
+
+    public void playMusic() {
+        System.out.println("Playing: " + music.getSong());
+    }
 
 }
